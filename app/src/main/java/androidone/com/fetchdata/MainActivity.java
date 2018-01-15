35aadcf40a29ecb7d1a78b7714c2f7946f99647e
DESKTOP-1HV52RQ\polah <harshitha08p@gmail.com>
2018-01-15 00:07:26 -0600

    Added Spinner,button to display movies according to Sort selection.

--- app/src/main/java/androidone/com/fetchdata/MainActivity.java
+++ app/src/main/java/androidone/com/fetchdata/MainActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -24,31 +29,66 @@ import static androidone.com.fetchdata.MovieDescriptionActivity.MOVIE_ID;
 
 public class MainActivity extends AppCompatActivity implements ViewAdapter.ItemclickListener {
     RecyclerView recyclerView;//created
+    public static String url="http://api.themoviedb.org/3/movie/now_playing?api_key=ce011fa6eca448775cf42ad614c86548";
     ArrayList<MovieItem> movieItems ;
+    Button filterButton;
+    Spinner spinner;
+    ArrayAdapter<String> sortValues;
     ViewAdapter viewAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Activity","\n onCreate()");
         setContentView(R.layout.activity_main);
         init();
-        fetchData();
-
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        onSortSelected();
+        Log.d("Activity","\n onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        onSortSelected();
+        Log.d("Activity","\n onStart()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        onSortSelected();
+        Log.d("Activity","\n onPause()");
+    }
+
+
     private void init()
     {
+        spinner=(Spinner)findViewById(R.id.SortSpinner);
+        sortValues=new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.sort_types));
+        sortValues.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(sortValues);
+        filterButton=(Button)findViewById(R.id.filterButton);
         recyclerView=(RecyclerView)findViewById(R.id.recycler_view_1);//initialized
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));//step 2
         movieItems=new ArrayList<MovieItem>();
         viewAdapter=new ViewAdapter(this,movieItems);
         recyclerView.setAdapter(viewAdapter);
+        Log.d("Activity1","\n init()");
     }
-    private void fetchData()
+
+    private void fetchData(String url)
     {
-        String url ="http://api.themoviedb.org/3/movie/now_playing?api_key=ce011fa6eca448775cf42ad614c86548";
+        Log.d("Activity1","\n fetchData()");
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Log.d("Response","Response :"+response);
+                Log.d("Activity1","\n onResponse()-AsyncTask");
                 try {
                     parseResponse(response);
                 } catch (JSONException e) {
@@ -66,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements ViewAdapter.Itemc
     }
     private void parseResponse(JSONObject response) throws JSONException {
         JSONArray myJson = JsonUtil.getJsonArray(response,"results");
-
+        Log.d("Activity1","\n parseResponse():to display the recyclerview data");
         for(int i=0;i<myJson.length();i++)
         {
 
@@ -86,9 +126,39 @@ public class MainActivity extends AppCompatActivity implements ViewAdapter.Itemc
 
     @Override
     public void onItemClick(String MovieId) {
+        Log.d("Activity1","\n onItemClick(): when movie is clicked");
         Intent i=new Intent(MainActivity.this,MovieDescriptionActivity.class);
         i.putExtra(MOVIE_ID,MovieId);
         startActivity(i);
+
+    }
+    private void onSortSelected()
+    {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==1)
+                {
+                    url="https://api.themoviedb.org/3/discover/movie?api_key=af7585c98115bbf645f463276b9b072b&language=en-US&sort_by=popularity.desc";
+                }
+                else if(i==2)
+                {
+                    url="https://api.themoviedb.org/3/discover/movie?api_key=af7585c98115bbf645f463276b9b072b&language=en-US&sort_by=vote_average.desc";
+                }
+                viewAdapter.notifyItemChanged(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fetchData(url);
+            }
+        });
 
     }
 }
